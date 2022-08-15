995a2ed19fa9cdc302fac5410a4202096c9fad74
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-15 03:45:44 -0500

    Instanciamos el Manger par ala auto

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ console.clear();
 
 import createExpressServer, { request, response } from "express";
 import { ConexionLoginBD } from "./src/ConexionBD/ConexionLoginBD.js";
-import { Autentificacion } from "./src/Modulo_AutentificacionAutorizacion/Autentificacion.js";
 import cuentaRuter from "./src/Rutas/login.js";
 import dotenv from "dotenv";
 
@@ -18,12 +17,11 @@ expressApp.use("/login.html", cuentaRuter);
 
 const main = async () =>{
     const crearConexionLogin = new ConexionLoginBD();
-    const auth = new Autentificacion();
     try {
         await crearConexionLogin.iniciarConexionMYSQL();
     } catch (error) {
         console.error("ERROR EN LA CONEXION A LA BD", error);
-        return;
+        throw error;
     }
     expressApp.listen(PUERTO, (request, response) =>{
         console.log("Servidor en el puerto: " + PUERTO);
