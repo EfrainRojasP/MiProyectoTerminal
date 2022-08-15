3a6687314360c74deb7d9543fb9258a9c3bdee40
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-15 03:47:41 -0500

    Login funcionanado

--- src/Rutas/login.js
+++ src/Rutas/login.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
+import { ManagerAutentificacionAutorizacion } from "../Modulo_AutentificacionAutorizacion/MangerAutentificacionAutorizacion.js";
 
 const cuentaRuter = Router();
 
-cuentaRuter.post("/Ingresar", (req, res) =>{
-    const body= req.body;
-    console.log(body);
-    res.json({rol: "1"});
-    return res.send();
+/*cuentaRuter.use((req, res, next) => {
+    mA = ManagerAutentificacionAutorizacion(e); 
+});*/
+
+cuentaRuter.post("/Ingresar", async (req, res) =>{
+    const {emailUser, passUser}= req.body;
+    const ma = new ManagerAutentificacionAutorizacion(emailUser, passUser);
+    const usuarioValido = await ma.validarEmailPass();
+    if(!usuarioValido){
+        console.log("USUARIO o PASS INCORRECTOS");
+        return res.sendStatus(404);
+    }
+    const rolUsuario = await ma.rolUsuario();
+    return res.json({
+        rolUsuario: rolUsuario
+    }).send();
 });
 
 export default cuentaRuter;
