435851faa15d1ee063d29e462b46ce5ce66ee231
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-18 01:42:55 -0500

    Almacenamos el token en el localsotrage

--- src/Publico/JavaScript/enviarDatosLogin.js
+++ src/Publico/JavaScript/enviarDatosLogin.js
@@ -3,19 +3,25 @@ const formulario = document.forms[0];
 const emailUser = document.getElementById("nombreUsario");
 const passUsuario = document.getElementById("passUsuario");
 
+function guardarToken(token) {
+    window.localStorage.setItem("access-token", token);
+}
+
 formulario.addEventListener("submit", async function (e) {
     e.preventDefault();
-    const login  = {
+    const login = {
         emailUser: emailUser.value,
         passUser: passUsuario.value
     };
-    console.log(login);
+    //console.log(login);
     const resJSON = await peticionFetch(login);
-    if(!resJSON){
+    console.log(resJSON.token);
+    guardarToken(resJSON.token);
+    if (!resJSON) {
         alert("USUARIO O CONTRASEÑA INCORRECTOS");
         return;
     }
-    if(resJSON.rolUsuario === 1){
+    if (resJSON.rolUsuario === 1) {
         formulario.action = "../GerenteRegional/index.html"
     } else {
         formulario.action = "../Encargado/EncargadoSubirReporte.html"
@@ -26,12 +32,12 @@ formulario.addEventListener("submit", async function (e) {
 async function peticionFetch(obj) {
     let response = await fetch('http://localhost:3200/login.html/Ingresar', {
         method: 'POST',
-     headers: {
+        headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify(obj)
     });
-    if(!response.ok){
+    if (!response.ok) {
         return false;
     }
 
