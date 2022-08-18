2e07fc7e5dac76a88f749e9a3d717ca7ca9a1b0f
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-18 01:43:57 -0500

    Enviamos el login si el usuario no existe

--- src/Rutas/redireccionarPOST.js
+++ src/Rutas/redireccionarPOST.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { send } from "process";
 import * as url from 'url';
+import { ConexionLoginBD } from "../ConexionBD/ConexionLoginBD.js";
+import { Token } from "../Modulo_AutentificacionAutorizacion/Token.js";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('../Publico', import.meta.url));
@@ -7,14 +10,30 @@ const __dirname = url.fileURLToPath(new URL('../Publico', import.meta.url));
 
 const redirecPOST = Router();
 
+
+redirecPOST.post("/validar", async (req, res) => {
+    const { authorization } = req.headers;
+    const userToken = authorization.substring(7) !== "null" ?
+                    authorization.substring(7) : false;
+    if (!authorization || !userToken) {
+        return res.status(404).send("LA SESION EXPIRO O NO ESTA LOGEADO");
+    }
+    const token = new Token();
+    const conexionBDLogin = new ConexionLoginBD();
+    const valido = await token.verificarToken(userToken, conexionBDLogin);
+    if (!valido) {
+        return res.status(404).send("LA SESION EXPIRO O NO ESTA LOGEADO");
+    }
+});
+
 redirecPOST.post("/GerenteRegional/index.html", (req, res) =>{
     //res.send(__dirname)
-    res.sendFile(__dirname + "/GerenteRegional/index.html");
+    return res.sendFile(__dirname + "/GerenteRegional/index.html");
 });
 
 redirecPOST.post("/Encargado/EncargadoSubirReporte.html", (req, res) =>{
     //res.send(__dirname)
-    res.sendFile(__dirname + "/Encargado/EncargadoSubirReporte.html");
+    return res.sendFile(__dirname + "/Encargado/EncargadoSubirReporte.html");
 });
 
 export default redirecPOST;
