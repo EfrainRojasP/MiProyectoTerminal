4630162792250911380cd5f4ca199408e9a961a6
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-19 08:30:30 -0500

    Añadimos una nueva ruta

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { ConexionLoginBD } from "./src/ConexionBD/ConexionLoginBD.js";
 import cuentaRuter from "./src/Rutas/login.js";
 import redirecPOST from "./src/Rutas/redireccionarPOST.js";
 import dotenv from "dotenv";
+import { ConexionVentasDB } from "./src/ConexionBD/ConexionVentasDB.js";
+import peticionesGerente from "./src/Modulo_Consultas/peticionesConsultas.js";
 
 
 dotenv.config();
@@ -19,11 +21,14 @@ expressApp.use(createExpressServer.text());
 expressApp.use(createExpressServer.static("./src/Publico"));
 expressApp.use("/login.html", cuentaRuter);
 expressApp.use("", redirecPOST);
+expressApp.use("/GrenteRegional", peticionesGerente);
 
 const main = async () => {
     const crearConexionLogin = new ConexionLoginBD();
+    const crearConexionVentas = new ConexionVentasDB();
     try {
         await crearConexionLogin.iniciarConexionMYSQL();
+        await crearConexionVentas.iniciarConexionMYSQL();
     } catch (error) {
         console.error("ERROR EN LA CONEXION A LA BD", error);
         throw error;
