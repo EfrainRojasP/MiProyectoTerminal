4473ccd77ff5946157c70137bdb16fdfa27e4eb7
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-19 09:06:39 -0500

    Corregimos errores

--- src/Publico/JavaScript/validar.js
+++ src/Publico/JavaScript/validar.js
@@ -1,9 +1,8 @@
 const token = window.localStorage.getItem("access-token");
 
-console.log("HOLAAAA");
-
-async function fetchValidar(){
-    let response = await fetch('http://localhost:3200/validar', {
+async function fetchValidar() {
+    let valido;
+    let response = await fetch('http://localhost:3200/Validar/Gerente', {
         method: 'POST',
         headers: {
             'Authorization': 'Bearer ' + token
@@ -11,21 +10,9 @@ async function fetchValidar(){
     });
     console.log(response);
     if (!response.ok) {
-        return false;
-    }
-
-    let result = await response.json();
-
-    return result;
-}
-
-async function main() {
-    const res = await fetchValidar();
-    if(!res){
         alert("La sesion expiro o no a ingresado")
         window.location.replace("http://localhost:3200/login.html");
     }
-    
 }
 
-main();
+fetchValidar();
