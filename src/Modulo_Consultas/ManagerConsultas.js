508c545a3443933b09bb559b2337841097c67773
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-22 01:35:23 -0500

    Agregamos los metodos para saber obtener las consultas de las opciones

--- src/Modulo_Consultas/ManagerConsultas.js
+++ src/Modulo_Consultas/ManagerConsultas.js
@@ -26,4 +26,61 @@ export class ManagerConsultas{
         return respuesta;
     }
 
+    /**
+     * 
+     * @returns Devulve las sucursales a cargo que tiene el gerente regional
+     */
+    async consultarSucursalesACargo(){
+        const conexionDBventas = new ConexionVentasDB();
+        const consultarVenta = new ConsultarVentas(conexionDBventas.getConexion());
+        const respuesta = await consultarVenta.consultarSucursalesACargo(this.GUID);
+        return respuesta;
+    }
+
+    /**
+     * 
+     * @returns Devulve el municipio o entidad de las sucursalales que tiene a cargo al Gerente Regional
+     */
+    async consultarSucursalesACargo_MunAlca(){
+        const conexionDBventas = new ConexionVentasDB();
+        const consultarVenta = new ConsultarVentas(conexionDBventas.getConexion());
+        const respuesta = await consultarVenta.consultarSucursalesACargo_MunAlca(this.GUID);
+        return respuesta;
+    }
+
+
+    /**
+     * 
+     * @returns Devulve las entidades de las sucursales que tiene a cargo al Gerente Regional
+     */
+    async consultarSucursalesACargo_Entidad(){
+        const conexionDBventas = new ConexionVentasDB();
+        const consultarVenta = new ConsultarVentas(conexionDBventas.getConexion());
+        const respuesta = await consultarVenta.consultarSucursalesACargo_Entidad(this.GUID);
+        return respuesta;
+    }
+
+    /**
+     * 
+     * @returns Devulve los productos en venta de las sucursales que tiene a cargo al Gerente Regional
+     */
+    async consultarSucursalesACargo_Producto(){
+        const conexionDBventas = new ConexionVentasDB();
+        const consultarVenta = new ConsultarVentas(conexionDBventas.getConexion());
+        const respuesta = await consultarVenta.consultarSucursalesACargo_ProductosVendidos(this.GUID);
+        return respuesta;
+    }
+
+    /**
+     * 
+     * @returns Devuelve el tipo de producto que tiene en venta las sucursales que tiene a cargo al Gerente Regional
+     */
+    async consultarSucursalesACargo_TipoProducto(){
+        const conexionDBventas = new ConexionVentasDB();
+        const consultarVenta = new ConsultarVentas(conexionDBventas.getConexion());
+        const respuesta = await consultarVenta.consultarSucursalesACargo_TipoProductosVendidos(this.GUID);
+        console.log("TIPO" + respuesta);
+        return respuesta;
+    }
+
 }
