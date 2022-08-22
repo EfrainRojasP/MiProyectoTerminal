6c667d5b25d3a209889e5eb381908c8c15132113
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-22 01:32:53 -0500

    Funcion para obtener el token

--- src/Ayudas/Ayudas.js
+++ src/Ayudas/Ayudas.js
@@ -1,3 +1,12 @@
 export function convertirRespuestaAObjeto(result){
     return Object.values(JSON.parse(JSON.stringify(result)));
 }
+
+/**
+ * 
+ * @param {token} authorization 
+ * @returns Devulve el token
+ */
+export function extraerToken(authorization){
+    return authorization.substring(7);
+}
