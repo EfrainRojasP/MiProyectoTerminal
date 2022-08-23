472369a1efb123d2ee552efce573b729a67fb7d9
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-23 00:59:25 -0500

    Añadmis un nuevo metodo para la consulta personalizada y añadimos documentoacion

--- src/Modulo_Consultas/Consultas.js
+++ src/Modulo_Consultas/Consultas.js
@@ -1,21 +1,21 @@
-import { convertirRespuestaAObjeto } from "../Ayudas/Ayudas.js";
+import { convertirRespuestaAObjeto, refectorizarFecha } from "../Ayudas/Ayudas.js";
 
 export class ConsultarVentas {
     conexionMYSQL;
 
 
     /**
-     * 
-     * @param {ConexionVentasDB} conexion
+     * Constructor el cual inicializa la conexion a la Base de datos
+     * @param {ConexionVentasDB} conexion 
      * Referencia a la Base de datos de Ventas
-     *  
      */
     constructor(conexion) {
         this.conexionMYSQL = conexion;
     }
 
     /**
-     * 
+     * Metodo que devuelve todas las ventas, dependiendo del GUID del gerente regional
+     * @param {String} GUID GUID del gerente regional
      * @returns Devuelve TODAS las VENTAS
      */
     consultarTodasVentas(GUID) {
@@ -32,17 +32,18 @@ export class ConsultarVentas {
                 }
                 //console.log(results);
                 const result = convertirRespuestaAObjeto(results[0]);
-                console.log(result);
+                //console.log(result);
                 return resolve(result);
             });
         });
     }
-/**
- * 
- * @param {String} GUID GUID del gerente regional
- * @returns Las tiendas a cargao que tiene el Genrente Regional
- */
-    consultarSucursalesACargo(GUID){
+
+    /**
+     * Metodo para saber cuales sucursales tiene a cargo el gerente regional
+     * @param {String} GUID GUID del gerente regional
+     * @returns Las tiendas a cargao que tiene el Genrente Regional
+     */
+    consultarSucursalesACargo(GUID) {
         return new Promise((resolve, reject) => {
             const stm = "CALL InformacionDeTiendas(?)";
             const query = this.conexionMYSQL.query(stm, GUID, function (error, results, fields) {
@@ -55,18 +56,19 @@ export class ConsultarVentas {
                 }
                 //console.log(results);
                 const result = convertirRespuestaAObjeto(results[0]);
-                console.log(results[0]["idSucursal"]);
+                //console.log(results[0]["idSucursal"]);
                 return resolve(result);
             });
         });
     }
 
     /**
-     * 
+     * Metodo para saber en que municipio o alcadia estan las sucursales que tiene a cargo el
+     * gerente regional
      * @param {String} GUID GUID del gerente regional
      * @returns Devulve el municipio o alcaldia de las sucursales que tiene a cargo al Gerente Regional
      */
-    consultarSucursalesACargo_MunAlca(GUID){
+    consultarSucursalesACargo_MunAlca(GUID) {
         return new Promise((resolve, reject) => {
             const stm = "CALL UbicacionTiendas_Municipio(?)";
             const query = this.conexionMYSQL.query(stm, GUID, function (error, results, fields) {
@@ -79,18 +81,19 @@ export class ConsultarVentas {
                 }
                 //console.log(results);
                 const result = convertirRespuestaAObjeto(results[0]);
-                console.log(results[0]);
+                //console.log(results[0]);
                 return resolve(result);
             });
         });
     }
 
     /**
-     * 
+     * Metodo para saber en que entidad estan las sucursales que tiene a cargo el
+     * gerente regional
      * @param {String} GUID GUID del gerente regional
      * @returns Devulve las entidades de las sucursales que tiene a cargo al Gerente Regional
      */
-    consultarSucursalesACargo_Entidad(GUID){
+    consultarSucursalesACargo_Entidad(GUID) {
         return new Promise((resolve, reject) => {
             const stm = "CALL UbicacionTiendas_Entidad(?)";
             const query = this.conexionMYSQL.query(stm, GUID, function (error, results, fields) {
@@ -103,18 +106,19 @@ export class ConsultarVentas {
                 }
                 //console.log(results);
                 const result = convertirRespuestaAObjeto(results[0]);
-                console.log(results[0]);
+                //console.log(results[0]);
                 return resolve(result);
             });
         });
     }
 
     /**
-     * 
+     * Metodo para saber cuales son los productos ventidos de cada sucursal que tiene a cargo el
+     * gerente regional
      * @param {String} GUID GUID del gerente regional
      * @returns Devulve los productos en venta de las sucursales que tiene a cargo al Gerente Regional
      */
-    consultarSucursalesACargo_ProductosVendidos(GUID){
+    consultarSucursalesACargo_ProductosVendidos(GUID) {
         return new Promise((resolve, reject) => {
             const stm = "CALL ProductosVentidos_Sucursal(?)";
             const query = this.conexionMYSQL.query(stm, GUID, function (error, results, fields) {
@@ -127,18 +131,19 @@ export class ConsultarVentas {
                 }
                 //console.log(results);
                 const result = convertirRespuestaAObjeto(results[0]);
-                console.log(results[0]);
+                //console.log(results[0]);
                 return resolve(result);
             });
         });
     }
 
     /**
-     * 
+     * Metodo para saber cuales son los tipos productos ventidos de cada sucursal que tiene a cargo el
+     * gerente regional
      * @param {String} GUID GUID del gerente regional
      * @returns Devuelve el tipo de producto que tiene en venta las sucursales que tiene a cargo al Gerente Regional
      */
-    consultarSucursalesACargo_TipoProductosVendidos(GUID){
+    consultarSucursalesACargo_TipoProductosVendidos(GUID) {
         return new Promise((resolve, reject) => {
             const stm = "CALL TipoProductosVentidos_Sucursal(?)";
             const query = this.conexionMYSQL.query(stm, GUID, function (error, results, fields) {
@@ -151,7 +156,35 @@ export class ConsultarVentas {
                 }
                 //console.log(results);
                 const result = convertirRespuestaAObjeto(results[0]);
-                console.log(results[0]);
+                //console.log(results[0]);
+                return resolve(result);
+            });
+        });
+    }
+
+    /**
+     * Metodo para la consulta personalizada
+     * @param {String} stm La consulta SQL que se hara a la vista "Ventas_Consultas"
+     * @param {Array} arrArgumentos Los argumentos de la consulta
+     * @returns Devulve el resultado de la consulta o false si no hay ninguna coicidencia
+     */
+    consultaPersonalizada(stm, arrArgumentos) {
+        console.log("CONSULTA" + stm);
+        console.log(arrArgumentos);
+
+        return new Promise((resolve, reject) => {
+            const query = this.conexionMYSQL.query(stm, arrArgumentos, function (error, results, fields) {
+                if (error) {
+                    return reject(error.stack)
+                }
+                //Si el email no esta en la BD se envia flase
+                if (results[0] === undefined) {
+                    return resolve(false);
+                }
+                //console.log(results);
+                let result = convertirRespuestaAObjeto(results);
+                result = refectorizarFecha(result);
+                console.log(result);
                 return resolve(result);
             });
         });
