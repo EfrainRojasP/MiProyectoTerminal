9fc2da153164ef7fe722cb774d4c14ca3397d02e
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-26 22:34:58 -0500

    Añimos una nueva ruta y un nuevo middleware

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ console.clear();
 
 import createExpressServer, { request, response } from "express";
 import { ConexionLoginBD } from "./src/ConexionBD/ConexionLoginBD.js";
-
+import fileUpload from "express-fileupload";
 import cuentaRuter from "./src/Rutas/login.js";
 import redirecPOST from "./src/Rutas/redireccionarPOST.js";
 import dotenv from "dotenv";
 import { ConexionVentasDB } from "./src/ConexionBD/ConexionVentasDB.js";
 import peticionesGerente from "./src/Modulo_Consultas/peticionesConsultas.js";
+import { subirInformacion } from "./src/Modulo_SubirInformacion/peticionesSubirInformacion.js";
 
 
 dotenv.config();
@@ -16,12 +17,18 @@ const PUERTO = process.env.PUERTO;
 
 expressApp.use(createExpressServer.json());
 expressApp.use(createExpressServer.text());
+expressApp.use(fileUpload());
 
-
+expressApp.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!')
+    next(err);
+});
 expressApp.use(createExpressServer.static("./src/Publico"));
 expressApp.use("/login.html", cuentaRuter);
 expressApp.use("", redirecPOST);
 expressApp.use("/GrenteRegional", peticionesGerente);
+expressApp.use("/Encargado", subirInformacion)
 
 const main = async () => {
     const crearConexionLogin = new ConexionLoginBD();
