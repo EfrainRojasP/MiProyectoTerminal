ccc8c86d98668d4331955ff9839a61ef2b5b7fff
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-09-12 00:17:11 -0500

    Doc

---
+++ src/Modulo_SubirInformacion/ManagerSubirInformacion.js
@@ -0,0 +1,53 @@
+import { extraerToken } from "../Ayudas/Ayudas.js";
+import { Token } from "../Modulo_AutentificacionAutorizacion/Token.js";
+import { Archivo } from "./Archivo.js";
+
+export class ManagerSubirInformacion{
+    token;
+    GUID;
+    archivo;
+
+    /**
+     * Constructor de ManagerArchivo
+     * @param {File} archivo Archivo que envio el usuario
+     */
+    constructor(archivo) {
+        this.token = new Token();
+        this.archivo = archivo;
+    }
+
+    /**
+     * 
+     * @param {authorization} token Token de usuario
+     * @returns True, si el archivo se guardo con exito.
+     * @returns Objeto, si el archivo no se guardo con exito o el token es 
+     * incrrecto o la extencion es invalida.
+     * 
+     */
+    async guardarArchivo(token){
+        const tokenEncargado = extraerToken(token);
+        const GUIDEncargado = await this.token.extraerGUID(tokenEncargado);
+        if(!GUIDEncargado){
+            return {
+                error: "Hubo un error, intentalo mas tarde"
+            };
+        }
+        const archivoEncargado = new Archivo(GUIDEncargado, this.archivo);
+        console.log("Tipo A: " + typeof this.archivo);
+        //console.log("EL ARCH " + archivoEncargado.de);
+        const extencionValida = archivoEncargado.validarArchivoExtencion(this.archivo.name);
+        if(!extencionValida){
+            return {
+                error: "Archivo invalido, solo se admiten archivos con extencion '.csv'"
+            };
+        }
+        const estadoGuardado = await archivoEncargado.guardarArchivo();
+        if(!estadoGuardado){
+            return {
+                error: "Parece que tubimos un problema al subir el archivo, intentalo mas tarde"
+            };
+        }
+        return estadoGuardado;
+    }
+
+}
