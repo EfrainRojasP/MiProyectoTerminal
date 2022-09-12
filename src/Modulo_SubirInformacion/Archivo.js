fe934e8231b90c90145303381bf5c17759286b74
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-09-12 00:15:53 -0500

    Doc

--- src/Modulo_SubirInformacion/Archivo.js
+++ src/Modulo_SubirInformacion/Archivo.js
@@ -1,6 +1,7 @@
 import Papa from "papaparse";
 import * as url from 'url';
 import path from "node:path";
+import * as fs from 'node:fs';
 
 export class Archivo {
     nombreArchivo;
@@ -10,7 +11,7 @@ export class Archivo {
     /**
      * Constructor, que inicializa el nombre del archivo
      * @param {String} nombreArchivo Nombre del archivo, el cual sera el GUID del encargado de la tienda
-     * @param {File} archivo Archivo que envia el usuario 
+     * @param {Object} archivo Archivo que envia el usuario 
      */
     constructor(nombreArchivo, archivo) {
         this.archivo = archivo;
@@ -50,4 +51,67 @@ export class Archivo {
         }
         return true;
     }
+
+    getArchivo(){
+        //..
+    }
+
+    /**
+     * Metodo GET, recupera el nombre del archivo
+     * @returns Devuelve el nombre del archivo
+     */
+    getNombreArchivo(){
+        return this.nombreArchivo;
+    }
+
+    /**
+     * Metodo GET, recupera el nombre del archivo
+     * @returns Devuelve la ubicacion del archivo
+     */
+    getUbicacionArchivo(){
+        return this.ubicacionArchivo;
+    }
+
+    /**
+     * Inicializa el arhivo
+     * @param {File} archivo 
+     */
+    setArchivo(archivo){
+        this.archivo = archivo;
+    }
+
+
+    /**
+     * Inicializa el nombre del archivo
+     * @param {String} nombreArchivo Nombre del archivo
+     */
+    setNombreArchivo(nombreArchivo){
+        this.nombreArchivo = nombreArchivo + ".csv";
+    }
+
+    /**
+     * Recupera la direccion en donde esta el archivo
+     * @returns Devuelve la direccion del archivo
+     */
+    recuperarArchivo(){
+        let file = fs.createReadStream(this.ubicacionArchivo);
+        return file;
+    }
+
+    /**
+     * Verifica si existe el archivo
+     * @returns Devuelve Ture si existe el archivo
+     * @returns Devuelve False si no existe el archivo
+     */
+    existeArchivo(){
+        return new Promise((resolve, reject) => {
+            if(fs.existsSync(this.ubicacionArchivo)){
+                resolve(true);
+            }
+            console.log(this.ubicacionArchivo);
+            resolve(false);    
+        });
+        
+    }
+
 }
